9f0a97b48fecec984de1cee5c261352691fff211
Halliwood <linxiaojia8802@126.com>
2019-04-13 19:49:36 +0800

    adjust the angle

--- playyard/unity/sceneDemo/Assets/RatPlayer.cs
+++ playyard/unity/sceneDemo/Assets/RatPlayer.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 public class RatPlayer : MonoBehaviour {
+    Tween modelTween;
+
+    ModelPlayer player;
+
+    bool isRunning;
     void Start() {
-        
+        modelTween = this.GetComponent<Tween>();
+		player = this.GetComponent<ModelPlayer>();
     }
+
+    void Update() {
+        if(isRunning) {
+           return; 
+        }
+        if(Random.Range(0, 10) > 6) {
+            modelTween.onFinished = onMoveFinished;
+            modelTween.moveTo(new Vector3(Random.Range(0f, 20f), Random.Range(0f, 20f), 0));
+            player.startMove();
+        }
+    }
+
+	void onMoveFinished() {
+		player.endMove();
+        isRunning = false;
+	}
 }
