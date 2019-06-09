dcdcce7e7a93654ec110ce7b81236ac19c5821c1
Halliwood <linxiaojia8802@126.com>
2019-06-09 16:49:03 +0800

    所有股票图表集合到index.html中

--- playyard/echart/stock_analysis/make.lua
+++ playyard/echart/stock_analysis/make.lua
@@ -88,134 +88,64 @@ local function toJson(dataMap)
 	return '{' .. table.concat(tmpArr, ', ') .. '}'
 end
 
-local function makeHtml(stockCode)
-    fileRoot = 'data/' .. stockCode .. '/'
-	-- 资产负债表
-	balancesheet = {}
-	parseSheet(fileRoot .. 'balancesheet.txt', balancesheet, '资产负债表')
+function makeAll()
+    local initData = ''
+	
+    for entry in lfs.dir('data')
+	do
+	    if entry ~= '.' and entry ~= '..' then
+		    stockCode = entry
+		    fileRoot = 'data/' .. stockCode .. '/'
+			-- 资产负债表
+			balancesheet = {}
+			parseSheet(fileRoot .. 'balancesheet.txt', balancesheet, '资产负债表')
 
-	-- 利润表
-	profitstatement = {}
-	parseSheet(fileRoot .. 'profitstatement.txt', profitstatement, '利润表')
+			-- 利润表
+			profitstatement = {}
+			parseSheet(fileRoot .. 'profitstatement.txt', profitstatement, '利润表')
+
+			-- 写入数据
+			local periodCnt = #profitstatement['利润表']
+			profitstatement['毛利润'] = {}
+			profitstatement['毛利率'] = {}
+			profitstatement['净利率'] = {}
+			for i = 1, periodCnt
+			do
+				profitstatement['毛利润'][i] = profitstatement['营业总收入'][i] - profitstatement['营业成本'][i]
+				profitstatement['毛利率'][i] = profitstatement['毛利润'][i] / profitstatement['营业总收入'][i]
+				profitstatement['净利率'][i] = profitstatement['净利润'][i] / profitstatement['营业总收入'][i]	
+			end
+
+			profitstatement['营业总收入同比'] = {}
+			profitstatement['归母净利润同比'] = {}
+			profitstatement['毛利率同比'] = {}
+			profitstatement['净利率同比'] = {}
+
+			balancesheet['存货同比'] = {}
+			for i = 1, periodCnt - 1
+			do
+				profitstatement['营业总收入同比'][i] = getYOY(profitstatement['营业总收入'][i], profitstatement['营业总收入'][i + 1])
+				profitstatement['归母净利润同比'][i] = getYOY(profitstatement['其中:归属于母公司股东的净利润'][i], profitstatement['其中:归属于母公司股东的净利润'][i + 1])
+				profitstatement['毛利率同比'][i] = getYOY(profitstatement['毛利率'][i], profitstatement['毛利率'][i + 1])
+				profitstatement['净利率同比'][i] = getYOY(profitstatement['净利率'][i], profitstatement['净利率'][i + 1])
+				
+				balancesheet['存货同比'][i] = getYOY(balancesheet['存货'][i], balancesheet['存货'][i + 1])
+			end
+			
+			initData = initData .. 'dataMap["' .. stockCode .. '"] = {"balancesheet": ' .. toJson(balancesheet) .. ', "profitstatement": ' .. toJson(profitstatement) .. '};\n'
+		end
+	end
 
 	-- 读入页面模板
 	tmplFile = io.open('tmpl.html', 'r')
 	htmlContent = tmplFile:read('*a')
 	tmplFile:close()
-
-	-- 写入数据
-	local periodCnt = #profitstatement['利润表']
-	profitstatement['毛利润'] = {}
-	profitstatement['毛利率'] = {}
-	profitstatement['净利率'] = {}
-	for i = 1, periodCnt
-	do
-		profitstatement['毛利润'][i] = profitstatement['营业总收入'][i] - profitstatement['营业成本'][i]
-		profitstatement['毛利率'][i] = profitstatement['毛利润'][i] / profitstatement['营业总收入'][i]
-		profitstatement['净利率'][i] = profitstatement['净利润'][i] / profitstatement['营业总收入'][i]	
-	end
-
-	profitstatement['营业总收入同比'] = {}
-	profitstatement['归母净利润同比'] = {}
-	profitstatement['毛利率同比'] = {}
-	profitstatement['净利率同比'] = {}
-
-	balancesheet['存货同比'] = {}
-	for i = 1, periodCnt - 1
-	do
-		profitstatement['营业总收入同比'][i] = getYOY(profitstatement['营业总收入'][i], profitstatement['营业总收入'][i + 1])
-		profitstatement['归母净利润同比'][i] = getYOY(profitstatement['其中:归属于母公司股东的净利润'][i], profitstatement['其中:归属于母公司股东的净利润'][i + 1])
-		profitstatement['毛利率同比'][i] = getYOY(profitstatement['毛利率'][i], profitstatement['毛利率'][i + 1])
-		profitstatement['净利率同比'][i] = getYOY(profitstatement['净利率'][i], profitstatement['净利率'][i + 1])
-		
-		balancesheet['存货同比'][i] = getYOY(balancesheet['存货'][i], balancesheet['存货'][i + 1])
-	end
 	
-	local initData = 'dataMap["' .. stockCode .. '"] = {"balancesheet": ' .. toJson(balancesheet) .. ', "profitstatement": ' .. toJson(profitstatement) .. '};\n'
 	htmlContent = string.gsub(htmlContent, '<initData>', initData)
 
-	local periods = ''
-	local shortTermLoans = ''
-	local longTermLoans = ''
-	local monetaryCapitals = ''
-	
-	local longTermLoadType = '长期借款'
-	if not(balancesheet['长期借款']) and balancesheet['应付债券']
-	then
-	    longTermLoadType = '应付债券'
-	end
-	for i = 1, periodCnt
-	do
-		if not(periods == '')
-		then
-			periods = ', ' .. periods
-			shortTermLoans = ', ' .. shortTermLoans
-			longTermLoans = ', ' .. longTermLoans
-			monetaryCapitals = ', ' .. monetaryCapitals
-		end
-		periods = '\'' .. balancesheet['资产负债表'][i] .. '\'' .. periods
-		shortTermLoans = balancesheet['短期借款'][i] .. shortTermLoans
-		if(balancesheet['长期借款'])
-		then
-		    longTermLoans = balancesheet['长期借款'][i] .. longTermLoans
-		elseif(balancesheet['应付债券'])
-		then
-		    longTermLoans = balancesheet['应付债券'][i] .. longTermLoans
-		else
-		    longTermLoans = 0 .. longTermLoans
-		end
-		monetaryCapitals = balancesheet['货币资金'][i] .. monetaryCapitals
-	end
-
-	local periodsYOY = ''
-	local incomeYOYs = ''
-	local netProfitYOYs = ''
-	local grossProfitMarginYOYs = ''
-	local netProfitMarginYOYs = ''
-	local inventoryYOYS = ''
-	for i = 1, periodCnt - 1
-	do
-		if not(periodsYOY == '')
-		then
-			periodsYOY = ', ' .. periodsYOY
-			incomeYOYs = ', ' .. incomeYOYs
-			netProfitYOYs = ', ' .. netProfitYOYs
-			grossProfitMarginYOYs = ', ' .. grossProfitMarginYOYs
-			netProfitMarginYOYs = ', ' .. netProfitMarginYOYs
-			inventoryYOYS = ', ' .. inventoryYOYS
-		end
-		periodsYOY = '\'' .. balancesheet['资产负债表'][i] .. '\'' .. periodsYOY
-		incomeYOYs = string.format('%.2f', profitstatement['营业总收入同比'][i] * 100) .. incomeYOYs
-		netProfitYOYs = string.format('%.2f', profitstatement['归母净利润同比'][i] * 100) .. netProfitYOYs
-		grossProfitMarginYOYs = string.format('%.2f', profitstatement['毛利率同比'][i] * 100) .. grossProfitMarginYOYs
-		netProfitMarginYOYs = string.format('%.2f', profitstatement['净利率同比'][i] * 100) .. netProfitMarginYOYs
-		inventoryYOYS = string.format('%.2f', balancesheet['存货同比'][i] * 100) .. inventoryYOYS
-	end
-
-	htmlContent = string.gsub(htmlContent, '<periods>', periods)
-	htmlContent = string.gsub(htmlContent, '<shortTermLoans>', shortTermLoans)
-	htmlContent = string.gsub(htmlContent, '<longTermLoadType>', longTermLoadType)
-	htmlContent = string.gsub(htmlContent, '<longTermLoans>', longTermLoans)
-	htmlContent = string.gsub(htmlContent, '<monetaryCapitals>', monetaryCapitals)
-	htmlContent = string.gsub(htmlContent, '<periodsYOY>', periodsYOY)
-	htmlContent = string.gsub(htmlContent, '<incomeYOYs>', incomeYOYs)
-	htmlContent = string.gsub(htmlContent, '<netProfitYOYs>', netProfitYOYs)
-	htmlContent = string.gsub(htmlContent, '<grossProfitMarginYOYs>', grossProfitMarginYOYs)
-	htmlContent = string.gsub(htmlContent, '<netProfitMarginYOYs>', netProfitMarginYOYs)
-	htmlContent = string.gsub(htmlContent, '<inventoryYOYS>', inventoryYOYS)
-
 	-- 生成页面
-	htmlFile = io.open(stockCode .. '.html', 'w')
+	htmlFile = io.open('index.html', 'w')
 	io.output(htmlFile)
 	io.write(htmlContent)
 	io.close(htmlFile)
-end
-
-function makeAll()
-    for entry in lfs.dir('data')
-	do
-	    if entry ~= '.' and entry ~= '..' then
-		    makeHtml(entry)
-		end
-	end
 end
